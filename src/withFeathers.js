9b25e33c566291fa8d8685a5366b8bf3494f1231
Frederic Rey <Frederic.Rey.Pro@gmail.com>
2016-09-05 23:54:19 +0100

    - withFeathers updated so that feathers services can be passed as props

--- src/withFeathers.js
+++ src/withFeathers.js
@@ -1,19 +1,42 @@
 import React, { PropTypes } from 'react';
 
-export default function withFeathers(WrappedComponent) {
-  const wrappedComponentName = WrappedComponent.displayName
+export default function withFeathers(mapServiceToProps) {
+  return function wrapInWithFeathers(WrappedComponent) {
+    const wrappedComponentName = WrappedComponent.displayName
     || WrappedComponent.name
     || 'Component';
-  const displayName = `withFeathers(${wrappedComponentName})`;
+    const displayName = `withFeathers(${wrappedComponentName})`;
 
-  const contextTypes = {
-    feathers: PropTypes.object,
+    const contextTypes = {
+      feathers: PropTypes.object,
+    };
+
+    function WithFeathers(props, context) {
+      const serviceNameSuffix = 'Service';
+      const feathersServices = {};
+
+      if (mapServiceToProps) {
+        if (Array.isArray(mapServiceToProps)) {
+          if (mapServiceToProps.length) {
+            for (const serviceName of mapServiceToProps) {
+              Object.defineProperty(feathersServices, `${serviceName}${serviceNameSuffix}`, {
+                value: context.feathers.service(serviceName),
+                enumerable: true,
+              });
+            }
+          } else {
+            throw new Error('mapServiceToProps is empty and consequently useless.');
+          }
+        } else {
+          throw new Error(`mapServiceToProps should be an Array of service names but
+            an ${typeof mapServiceToProps} is found instead`);
+        }
+      }
+
+      return <WrappedComponent feathers={context.feathers} {...props} {...feathersServices}/>;
+    }
+    WithFeathers.displayName = displayName;
+    WithFeathers.contextTypes = contextTypes;
+    return WithFeathers;
   };
-
-  function WithFeathers(props, context) {
-    return <WrappedComponent feathers={context.feathers} {...props}/>;
-  }
-  WithFeathers.displayName = displayName;
-  WithFeathers.contextTypes = contextTypes;
-  return WithFeathers;
 }
